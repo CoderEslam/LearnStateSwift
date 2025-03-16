256ee35f7c6aab66155a4c819678c6139b3a4980
CoderEslam <eslamghazy600@gmail.com>
2025-03-16 17:05:12 +0100

    Initial Commit

---
+++ LearnStateSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnStateSwift
+//
+//  Created by Eslam Ghazy on 16/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnStateSwift/LearnStateSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnStateSwiftApp.swift
+//  LearnStateSwift
+//
+//  Created by Eslam Ghazy on 16/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnStateSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
